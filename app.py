8e1b21956c9b630daa64537cae36034c278a49b5
Debabrata Bepari <debabratabepari2@gmail.com>
2024-08-03 19:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import google.generativeai as genai 
 from PIL import Image
 
-genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key= os.getenv(st.secrets["GOOGLE_API_KEY"]))
 
 def get_gemini_response(input_prompt, image):
 
